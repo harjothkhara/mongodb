a47114aad166cdb23a1500a6233fc3c5ef4ec23c
harjoth <harjoth.khara@gmail.com>
2019-08-19 17:22:54 -0700

    intro to mocha testing - mocha is a testing framework, use it to perform tests within our app, make sure everything works correctly. test our connection with the database - creating records, reading records, updating records, deleting records. added mocha dependency -> npm run test

---
+++ test/demo_test.js
@@ -0,0 +1,11 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+describe('some demo test', () => {
+
+  // create tests
+  it('adds two number together', () => {
+    assert(2 + 3 === 5);
+  });
+
+});
